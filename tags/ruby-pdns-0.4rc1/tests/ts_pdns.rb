65672cef8ef3e33ad97862ad926ea05a75cbc2c0
rip@devco.net <rip@devco.net@7390e61a-850e-11de-8eb9-d39840a433f0>
2009-08-24 12:16:34 +0000

    tag 0.4rc1

---
+++ tags/ruby-pdns-0.4rc1/tests/ts_pdns.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+require 'test/unit'
+require 'tc_array.rb'
+require 'tc_config.rb'
+require 'tc_geoip.rb'
+require 'tc_log.rb'
+require 'tc_resolvers.rb'
+require 'tc_response.rb'
