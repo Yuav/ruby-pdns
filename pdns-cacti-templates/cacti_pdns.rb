776c8bbb33a831f3d2c787629c9bb68cab3b5ece
rip <rip@devco.net>
2009-10-03 18:22:40 +0000

    Update issue 29 Create branch for 0.5

--- pdns-cacti-templates/cacti_pdns.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/ruby
-# Simple script which converts PowerDNS stats into Cacti standard data input format
-
-output = `/sbin/service pdns dump`
-print output.gsub("=", ":").split(",").join(" ")
